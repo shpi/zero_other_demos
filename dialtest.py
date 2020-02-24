28650094ed8645e1050b1d278c1605c49e5ac7f0
SHPI <lh@shpi.de>
2020-02-24 04:08:26 +0100

    added dialtest

--- dialtest.py
+++ dialtest.py
@@ -18,8 +18,7 @@ try:
     bus.write([0x70, 0b00000000], 0x5c)
 except:
     print('Error: no touchscreen found')
-    TOUCHADDR = False
-
+    
 
 def touch():
 
@@ -31,7 +30,7 @@ def touch():
                 if y1 == 0:
                     y1 = 1
                 if ((-401 < x1 < 401) & (-241 < y1 < 241)):
-                        print(x1,y1)
+                        #print(x1,y1)
                         return x1, y1  # compensate position to match with PI3D
                 else:
                         time.sleep(0.01)
@@ -51,25 +50,22 @@ MATSH = pi3d.Shader("mat_flat")
 
 shape = []
 shape2 = []
-value = 100
-
 
 
 for x in range(-135,135):
- if value < x:
-   shape2.append((200* sin(radians(x)), 200* cos(radians(x)),2 ))
- else:
-  if x % 10 == 0:
-   shape.append((170* sin(radians(x)) , 170 * cos(radians(x)), 2))
-   shape.append((230* sin(radians(x)) , 230 * cos(radians(x)), 2))
+   if x % 3 == 0:
+    shape2.append((200* sin(radians(x)), 200* cos(radians(x)), 2))
+   if x % 5 == 0:
+    shape.append((180* sin(radians(x)) , 180 * cos(radians(x)), 2))
+    shape.append((220* sin(radians(x)) , 220 * cos(radians(x)), 2))
 
 
-line = pi3d.Lines(vertices=shape,camera=CAMERA, line_width=10, strip=False)
+line = pi3d.Lines(vertices=shape,camera=CAMERA, line_width=5, strip=False)
 line.set_shader(MATSH)
 line.set_material((1, 1, 1))
 line.set_alpha(0.8)
 
-line2 = pi3d.Lines(vertices=shape2,camera=CAMERA, line_width=35, strip=True)
+line2 = pi3d.Lines(vertices=shape2,camera=CAMERA, line_width=25, strip=True)
 line2.set_shader(MATSH)
 line2.set_material((1, 1, 1))
 line2.set_alpha(0.2)
@@ -77,7 +73,7 @@ line2.set_alpha(0.2)
 x2 = 400
 y2 = -100
 SHADER = pi3d.Shader("uv_flat")
-actval= pi3d.FixedString('opensans.ttf', '0', font_size=65,shadow_radius=0, 
+actval= pi3d.FixedString('opensans.ttf', '0', font_size=30,shadow_radius=0, 
                         background_color=(0,0,0,0), color= (255,255,255,255),
                         camera=CAMERA, shader=SHADER, f_type='SMOOTH')
 
@@ -90,52 +86,32 @@ while DISPLAY.loop_running():
  if (gpio.input(26)):
   x2, y2 = touch()
 
- dot = pi3d.Disk(radius=8, sides=20, z=0.1, rx=90,x= x2,y=y2, camera=CAMERA) 
- dot.set_shader(MATSH)
- dot.set_material((1, 0, 0))
- dot.set_alpha(1)
 
- line3 = pi3d.Lines(vertices=[(0,0,2),(x2,y2,2)], line_width=2, strip=True)
- degree = degrees(atan(x2/y2))
+ #line3 = pi3d.Lines(vertices=[(0,0,2),(x2,y2,2)], line_width=2, strip=True)
+ degree = int(degrees(atan(x2/y2)))
 
  if y2 < 0:
   degree += 180
+  if x2 < 0:
+   degree -= 360
 
- if y2 < 0 and x2 < 0:
-  degree -= 360
-
- degree = int(degree)
+ 
  if degree > 135:  degree = 135
  if degree < -135: degree = -135
 
  actval = (degree + 135) / 2.7
  rgbval = round((degree + 135) / 270,2)
- if degree < 135:
-  shape2 =[]
-  for x in range(degree,135):
-
-    shape2.append((200* sin(radians(x)), 200* cos(radians(x)),2 ))
- if degree > -125:
-  shape = []
-  for x in range(-135,degree):
-   if x % 10 == 0:
-    shape.append((170* sin(radians(x)) , 170 * cos(radians(x)), 2))
-    shape.append((230* sin(radians(x)) , 230 * cos(radians(x)), 2))
-
-
- line = pi3d.Lines(vertices=shape,camera=CAMERA, line_width=10, strip=False)
+ 
+ 
+ line = pi3d.Lines(vertices=shape[0:((degree+138)*2)//5],camera=CAMERA, line_width=5, strip=False)
  line.set_shader(MATSH)
  line.set_material((rgbval, 0, 1-rgbval))
  line.set_alpha(0.8)
 
- line2 = pi3d.Lines(vertices=shape2,camera=CAMERA, line_width=35, strip=True)
+ line2 = pi3d.Lines(vertices=shape2[(degree+134)//3:],camera=CAMERA, line_width=5, strip=True)
  line2.set_shader(MATSH)
  line2.set_material((1, 1, 1))
  line2.set_alpha(0.2)
-
-
-
-
 
  actval= pi3d.FixedString('opensans.ttf', str(int(actval))+'%', font_size=65,shadow_radius=0, 
                         background_color=(0,0,0,0), color= (255,255,255,255),
@@ -143,23 +119,19 @@ while DISPLAY.loop_running():
  actval.sprite.position(0, 0, 1)
 
 
-
-
-
  x1 = 200* sin(radians(degree))
  y1 = 200* cos(radians(degree))
 
  dot2= pi3d.Disk(radius=8, sides=20, z=0.1, rx=90,x=x1, y=y1, camera=CAMERA)
  dot2.set_shader(MATSH)
- dot2.set_material((1, 0, 0))
+ dot2.set_material((rgbval, 0, 1-rgbval))
  dot2.set_alpha(1)
 
 
-
+ 
  line.draw() 
- line2.draw()
- dot.draw()
  dot2.draw()
- line3.draw()
+ line2.draw()
+ #line3.draw()
  actval.draw()
 DISPLAY.destroy()
