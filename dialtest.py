37c403994f81df8af5c17c6c0fa4c9eb45c77b10
SHPI <lh@shpi.de>
2020-02-24 12:05:56 +0100

    added dialtest.py

--- dialtest.py
+++ dialtest.py
@@ -54,28 +54,41 @@ shape2 = []
 
 for x in range(-135,135):
    if x % 3 == 0:
-    shape2.append((200* sin(radians(x)), 200* cos(radians(x)), 2))
+    shape2.append((220* sin(radians(x)), 220* cos(radians(x)), 2))
    if x % 5 == 0:
-    shape.append((180* sin(radians(x)) , 180 * cos(radians(x)), 2))
-    shape.append((220* sin(radians(x)) , 220 * cos(radians(x)), 2))
+    shape.append((200* sin(radians(x)) , 200 * cos(radians(x)), 2))
+    shape.append((240* sin(radians(x)) , 240 * cos(radians(x)), 2))
 
+degree = 0
 
-line = pi3d.Lines(vertices=shape,camera=CAMERA, line_width=5, strip=False)
+line = pi3d.Lines(vertices=shape[0:((degree+138)*2)//5],camera=CAMERA, line_width=5, strip=False)
 line.set_shader(MATSH)
 line.set_material((1, 1, 1))
 line.set_alpha(0.8)
 
-line2 = pi3d.Lines(vertices=shape2,camera=CAMERA, line_width=25, strip=True)
+line2 = pi3d.Lines(vertices=shape2[(degree+134)//3:],camera=CAMERA, line_width=5, strip=True)
 line2.set_shader(MATSH)
 line2.set_material((1, 1, 1))
 line2.set_alpha(0.2)
 
-x2 = 400
-y2 = -100
+
 SHADER = pi3d.Shader("uv_flat")
 actval= pi3d.FixedString('opensans.ttf', '0', font_size=30,shadow_radius=0, 
                         background_color=(0,0,0,0), color= (255,255,255,255),
                         camera=CAMERA, shader=SHADER, f_type='SMOOTH')
+
+x1 = 220* sin(radians(degree))
+y1 = 220* cos(radians(degree))
+
+
+
+dot2= pi3d.Disk(radius=8, sides=20, z=0.1, rx=90,x=x1, y=y1, camera=CAMERA)
+dot2.set_shader(MATSH)
+dot2.set_material((1, 1, 1))
+dot2.set_alpha(1)
+
+
+
 
 while DISPLAY.loop_running():
 
@@ -84,48 +97,49 @@ while DISPLAY.loop_running():
 
 
  if (gpio.input(26)):
-  x2, y2 = touch()
+  x3, y3 = touch()
+  
+  if (x1 - 80) < x3 and x3  < (x1 + 80):
+    if (y1 - 80) < y3 and y3  < (y1 + 80):
+     
+     x1, y1 = x3, y3
+     #line3 = pi3d.Lines(vertices=[(0,0,2),(x2,y2,2)], line_width=2, strip=True)
+     degree = int(degrees(atan(x1/y1)))
 
-
- #line3 = pi3d.Lines(vertices=[(0,0,2),(x2,y2,2)], line_width=2, strip=True)
- degree = int(degrees(atan(x2/y2)))
-
- if y2 < 0:
-  degree += 180
-  if x2 < 0:
-   degree -= 360
-
+     if y1 < 0:
+       degree += 180
+       if x1 < 0:
+         degree -= 360
  
- if degree > 135:  degree = 135
- if degree < -135: degree = -135
+     if degree > 135:  degree = 135
+     if degree < -135: degree = -135
 
- actval = (degree + 135) / 2.7
- rgbval = round((degree + 135) / 270,2)
+     actval = (degree + 135) / 2.7
+     rgbval = round((degree + 135) / 270,2)
  
- 
- line = pi3d.Lines(vertices=shape[0:((degree+138)*2)//5],camera=CAMERA, line_width=5, strip=False)
- line.set_shader(MATSH)
- line.set_material((rgbval, 0, 1-rgbval))
- line.set_alpha(0.8)
+     line = pi3d.Lines(vertices=shape[0:((degree+138)*2)//5],camera=CAMERA, line_width=5, strip=False)
+     line.set_shader(MATSH)
+     line.set_material((rgbval, 0, 1-rgbval))
+     line.set_alpha(0.8)
 
- line2 = pi3d.Lines(vertices=shape2[(degree+134)//3:],camera=CAMERA, line_width=5, strip=True)
- line2.set_shader(MATSH)
- line2.set_material((1, 1, 1))
- line2.set_alpha(0.2)
+     line2 = pi3d.Lines(vertices=shape2[(degree+134)//3:],camera=CAMERA, line_width=5, strip=True)
+     line2.set_shader(MATSH)
+     line2.set_material((1, 1, 1))
+     line2.set_alpha(0.2)
 
- actval= pi3d.FixedString('opensans.ttf', str(int(actval))+'%', font_size=65,shadow_radius=0, 
+     actval= pi3d.FixedString('opensans.ttf', str(int(actval))+'%', font_size=65,shadow_radius=0, 
                         background_color=(0,0,0,0), color= (255,255,255,255),
                         camera=CAMERA, shader=SHADER, f_type='SMOOTH')
- actval.sprite.position(0, 0, 1)
+     actval.sprite.position(0, 0, 1)
 
 
- x1 = 200* sin(radians(degree))
- y1 = 200* cos(radians(degree))
+     x1 = 220* sin(radians(degree))
+     y1 = 220* cos(radians(degree))
 
- dot2= pi3d.Disk(radius=8, sides=20, z=0.1, rx=90,x=x1, y=y1, camera=CAMERA)
- dot2.set_shader(MATSH)
- dot2.set_material((rgbval, 0, 1-rgbval))
- dot2.set_alpha(1)
+     dot2= pi3d.Disk(radius=8, sides=20, z=0.1, rx=90,x=x1, y=y1, camera=CAMERA)
+     dot2.set_shader(MATSH)
+     dot2.set_material((rgbval, 0, 1-rgbval))
+     dot2.set_alpha(1)
 
 
  
