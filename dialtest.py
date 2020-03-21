7e02c04d85709d97a808764f3db30b917771ba3b
SHPI <lh@shpi.de>
2020-03-22 00:56:05 +0100

    modified dialtest.py

--- dialtest.py
+++ dialtest.py
@@ -5,6 +5,7 @@ import RPi.GPIO as gpio
 import i2c
 import time
 
+
 class Dial(object):
     def __init__(self, angle_fr=-135, angle_to=135, step=5, outer=240, inner=200,
                 min_t=15, max_t=35, shader=None, camera=None):
@@ -31,39 +32,40 @@ class Dial(object):
         except:
             print('Error: no touchscreen found')
 
-        solid_verts = []
         tick_verts = []
-
+        dial_verts = []
+        # first make tick vertices
         for x in range(self.angle_fr, self.angle_to, self.step):
             (s, c) = (sin(radians(x)), cos(radians(x))) # re-use for brevity below
-            solid_verts.append((self.mid * s, self.mid * c, 2))
-            tick_verts.append((self.inner * s, self.inner * c, 0.1))
-            tick_verts.append((self.outer * s, self.outer * c, 0.1))
+            tick_verts.extend([(self.inner * s, self.inner * c, 0.1),
+                                (self.outer * s, self.outer * c, 0.1)])
+            dial_verts.append((self.mid * s, self.mid * c, 2.0))
 
         if shader is None:
             shader = pi3d.Shader('mat_flat')
         if camera is None:
             camera = pi3d.Camera(is_3d=False)
+        uv_shader = pi3d.Shader('uv_flat')
 
-        self.ticks = pi3d.Lines(vertices=tick_verts, camera=camera, line_width=5, strip=False)
+        tex = pi3d.Texture('color_gradient.jpg')
+
+        self.ticks = pi3d.PolygonLines(camera=camera, vertices=tick_verts, strip=False, line_width=5)
+        
         self.ticks.set_shader(shader)
-        self.ticks.set_material((1, 1, 1))
         self.ticks.set_alpha(0.8)
 
-        self.sensorticks = pi3d.Lines(vertices=tick_verts, camera=camera, line_width=8, strip=False)
+        self.sensorticks = pi3d.PolygonLines(camera=camera, vertices=tick_verts, line_width=5, strip=False)
         self.sensorticks.set_shader(shader)
-        self.sensorticks.set_material((1, 1, 1))
-        self.sensorticks.set_alpha(1)
 
-        self.bline = pi3d.Lines(vertices=solid_verts, camera=camera, line_width=40, strip=True)
-        self.bline.set_shader(shader)
-        self.bline.set_material((0, 1, 0))
-        self.bline.set_alpha(0.4)
+        self.bline = pi3d.PolygonLines(camera=camera, vertices=dial_verts, line_width=40)
+        self.bline.set_textures([tex])
+        self.bline.set_alpha(0.8)
+        self.bline.set_shader(uv_shader)
+        self.bline.set_material((0.5,0.5,0.5))
 
-        self.dial = pi3d.Lines(vertices=solid_verts, camera=camera, line_width=5, strip=True)
-        self.dial.set_shader(shader)
-        self.dial.set_material((1, 1, 1))
+        self.dial = pi3d.PolygonLines(camera=camera, vertices=dial_verts, line_width=8)
         self.dial.set_alpha(0.2)
+        self.dial.set_shader(shader)
 
         font = pi3d.Font('opensans.ttf', codepoints='0123456789.-°', grid_size=5)
         self.actval = pi3d.PointText(font, camera, max_chars=10, point_size=100) 
@@ -76,10 +78,15 @@ class Dial(object):
         self.dot2.set_material((1, 1, 1))
         self.dot2_alpha = 1.0
 
-        self.x1, self.y1 = -5, 10
-        self.set_flag = True
-        self.value = 20.0
+
+        self.value = 25.0
         self.sensorvalue = 18.0
+        degree = (self.angle_fr +  (self.angle_to - self.angle_fr) * (self.value - self.min_t)
+                                                            / (self.max_t - self.min_t))
+        self.x1 = self.mid * sin(radians(degree))
+        self.y1 = self.mid * cos(radians(degree))
+
+
 
     def touch(self):
         try:
@@ -99,13 +106,8 @@ class Dial(object):
             return self.touch()
 
     def check_touch(self):
-        if self.set_flag or gpio.input(26):
-            if self.set_flag:
-                x3, y3 = self.x1, self.y1
-                self.x1 *= 0.9 # i.e. just inside but at the same angle
-                self.y1 *= 0.9
-            else:
-                x3, y3 = self.touch()
+           if gpio.input(26):
+            x3, y3 = self.touch()
             if ((self.x1 - 80) < x3 and x3  < (self.x1 + 80) and
                 (self.y1 - 80) < y3 and y3  < (self.y1 + 80)):
                 self.x1, self.y1 = x3, y3
@@ -117,56 +119,45 @@ class Dial(object):
 
                 self.value = (self.min_t + (degree - self.angle_fr)
                              / (self.angle_to - self.angle_fr) * (self.max_t - self.min_t))
-                rgbval = round((degree - self.angle_fr) / (self.angle_to - self.angle_fr), 2) # rgbval 0.0 - 1.0
+                sensordegree = (self.angle_fr +  (self.angle_to - self.angle_fr) * (self.sensorvalue - self.min_t)
+                                                            / (self.max_t - self.min_t))
+                if  self.value > self.sensorvalue:
+                    self.ticks.set_material((1, 0 , 0))
+                else:
+                    self.ticks.set_material((0,0,1))
+                """ move z values for shapes. The z tuple values represent:
+                    0 to the set point (cut_n), 0 to the sensor point (cut_s),
+                    between the two points, everwhere else. None prevents the section being adjusted
+                    from a previously set value
+                """
+                updateelements = []
 
-                ## move vertices z value for shapes
-                for (line_shape, z_first, z_second, rgb) in (
-                        (self.ticks, 1.0, -1.0, (rgbval, 0.0, 1.0 - rgbval)),
-                        (self.dial, -1.0, 0.1, (1.0, 1.0, 1.0)),
-                        (self.bline, 2.0, -1.0, (0.0, 1.0, 0.0))):
+                updateelements.append((self.ticks, (-1.0, -1.0, 0.1, -1.0)))
+                updateelements.append((self.sensorticks, (0.2, None, None, -1.0)))
+                updateelements.append((self.dial, (-1.0, -1.0, 0.1, 0.1)))
+                updateelements.append((self.bline, (None, 0.3, None, -1.0)))
+
+                for (line_shape, z) in updateelements:
                     b = line_shape.buf[0]
                     v = b.array_buffer
-                    cut_n = int((degree - self.angle_fr) / (self.angle_to - self.angle_fr) * len(v))
+                    cut_n = int((degree - self.angle_fr) / (self.angle_to - self.angle_fr) * len(v) / 4) * 4
                     if cut_n >= len(v):
                         cut_n = len(v) - 1
-                    v[:cut_n:,2] = z_first  # show points by moving away
-                    v[cut_n:, 2] = z_second # hide points by moving behind near plane!
-                    b.re_init()
-                    b.set_material(rgb)
-
-
-                for (line_shape, z_first, z_second, z_third) in [(self.sensorticks, -1.0, 0.1, -1.0)]:
-                    b = line_shape.buf[0]
-                    v = b.array_buffer
-                    sensordegree = (self.angle_fr +  (self.angle_to - self.angle_fr) * (self.sensorvalue - self.min_t)
-                                                            / (self.max_t - self.min_t))
-                    cut_n = int((degree - self.angle_fr) / (self.angle_to - self.angle_fr) * len(v))
-                    cut_s = int((sensordegree - self.angle_fr) / (self.angle_to - self.angle_fr) * len(v))
-
+                    cut_s = int((sensordegree - self.angle_fr) / (self.angle_to - self.angle_fr) * len(v) / 4) * 4
                     if cut_s >= len(v):
                         cut_s = len(v) - 1
-
-                    if cut_n >= len(v):
-                        cut_n = len(v) - 1
-
-                    if sensordegree > degree:
-
-                     v[:cut_n:,2] = z_first  # show points by moving away
-                     v[cut_n:cut_s, 2] = z_second # hide points by moving behind near plane!
-                     v[cut_s:, 2] = z_third # hide points by moving behind near plane!
-                     rgb = (0,0,1)
-
-                    else:
-                     v[:cut_s:,2] = z_first  # show points by moving away
-                     v[cut_s:cut_n, 2] = z_second # hide points by moving behind near plane!
-                     v[cut_n:, 2] = z_third # hide points by moving behind near plane!
-                     rgb = (1,0,0)
-
+                    v[:, 2] = z[3]           # all set to the 'otherwise' value
+                    if z[0] is not None:
+                        v[:cut_s, 2] = z[0]  # set visibility up to sensor value
+                    if z[1] is not None:
+                        v[:cut_n, 2] = z[1]  # set vis up to set point value
+                    if z[2] is not None:     # set between cut_n and cut_s
+                        if cut_s > cut_n:
+                            v[cut_n:cut_s, 2] = z[2]
+                        else:
+                            v[cut_s:cut_n, 2] = z[2]
                     b.re_init()
-                    b.set_material(rgb)
-
-
-
+                    #b.set_material(rgb)
 
                 self.temp_block.set_text(text_format="{:4.1f}°".format(self.value))
                 self.actval.regen()
@@ -176,19 +167,28 @@ class Dial(object):
 
                 self.dot2.position(self.x1, self.y1, 0.5)
                 self.dot2_alpha = 1.0
+                self.ticks_alpha = 0.0
                 self.bline.draw()
-            self.set_flag = False
+            
 
     def draw(self):
-        if self.dot2_alpha > 0.0:
+        if self.dot2_alpha >= 0.0:
             self.dot2_alpha -= 0.01
             self.dot2.set_alpha(self.dot2_alpha)
             self.dot2.draw()
-            if self.dot2_alpha < 0.0:
-              self.ticks.set_material((1,1,1))
+            if self.dot2_alpha < 0:
+                self.temp_block.set_text(text_format="{:4.1f}°".format(self.sensorvalue))
+                self.actval.regen()
+                self.ticks_alpha = 0
         else:
-            self.sensorticks.draw()
-        self.ticks.draw()
+            if self.ticks_alpha <= 1.0:
+                self.ticks_alpha += 0.01
+                self.ticks.set_alpha(self.ticks_alpha)
+                if self.ticks_alpha >= 1.0:
+                    self.ticks_alpha = 0.5
+
+            self.ticks.draw()
+        self.sensorticks.draw()
         self.dial.draw()
         self.actval.draw()
 
@@ -205,19 +205,13 @@ class Dial(object):
 
 DISPLAY = pi3d.Display.create(layer=0, w=800, h=480, background=(0.0, 0.0, 0.0, 1.0),
                               frames_per_second=60, tk=False, samples=4)
-#CAMERA = pi3d.Camera(is_3d=False) # will create its own cam if one not passed
+CAMERA = pi3d.Camera(is_3d=False) # will create its own cam if one not passed
 
-dial = Dial()
+dial = Dial(camera=CAMERA)
 
 i = 0
 while DISPLAY.loop_running():
     dial.check_touch()
     dial.draw()
-
-    i += 1
-    if i % 1000 == 0:
-        dial.set_value(25.0)
-    if i % 250 == 0:
-        print(dial.get_value())
 
 DISPLAY.destroy()
